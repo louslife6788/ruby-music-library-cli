573f0064ba18b304f5cb2ae3b0fc6ed754da00b1
luis leon <leonluis380@yahoo.com>
2021-07-29 20:24:55 -0400

    Done.

--- lib/Song.rb
+++ lib/Song.rb
@@ -42,6 +42,14 @@
         
     end
 
+    def self.find_by_name(name)
+        self.all.detect {|s| s.name == name}
+    end
+
+    def self.find_or_create_by_name(name)
+        self.find_by_name(name) || self.create(name)
+    end
+
 
 
 
